024dca4b2b3c5526bab4972a4643da958518b700
techsyhub <techsyhub@gmail.com>
2023-09-25 17:16:03 +0500

    Project Setup | database connection and redis setup

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
 require("dotenv").config();
 import { app } from "./app";
+import connectDB from "./utils/db";
 
 
 //create server
 app.listen(process.env.PORT , ()=>{
     console.log(`Server is connected with port ${process.env.PORT}`)
+    connectDB()
 })
